a9bdc82d236ffaabf2e1724e97d154055d06965b
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2017-04-17 15:42:27 +0200

    raw audio utils: implement data augmentation

--- dcase_framework/raw_audio_utils.py
+++ dcase_framework/raw_audio_utils.py
@@ -1,13 +1,16 @@
 import os
 import copy
 import numpy
+import importlib
+import muda
+import jams
 from .files import AudioFile
 from .features import FeatureContainer
 
 
 class RawAudioBatcher():
-    def __init__(self, split_files, _annotations, class_labels, batch_size=1, mono=True,
-                 desired_fs=22050, segment=True, frame_size_sec0=5.0, normalize=False):
+    def __init__(self, split_files, _annotations, class_labels, batch_size=1, mono=True, desired_fs=22050,
+                 segment=True, frame_size_sec0=5.0, normalize=False, augmentation_params=None):
         """
         RawAudioBatcher allows to create batches from audio data.
         It contains a generator method that can be used as input to the method fit_generator of a
@@ -38,8 +41,12 @@ class RawAudioBatcher():
             Normalize output values between 0 and 1. If mono is False, channels are normalized with the same value.
         """
         self.files = split_files
+        self.n_files = len(self.files)
         self.annotations = _annotations
-        self.batch_size = batch_size
+        if self.n_files < batch_size:
+            self.batch_size = self.n_files
+        else:
+            self.batch_size = batch_size
         self.mono = mono
         self.desired_fs = desired_fs
         self.normalize = normalize
@@ -61,6 +68,13 @@ class RawAudioBatcher():
         self.batch_annotations = {}
 
         self.n_batches = None
+
+        # data augmentation parameters
+        self.enable_augmentation = augmentation_params['enable']
+        if self.enable_augmentation:
+            self.augmentation_samples = None
+            self.augmentation_chain = self.create_augmentation_chain(augmentation_params)
+            # self.batch_size *= self.batch_size + self.batch_size * self.augmentation_samples
 
     def get_item_shape(self):
         for f in self.files:
@@ -167,6 +181,9 @@ class RawAudioBatcher():
                     norm_val = numpy.max(numpy.max(numpy.abs(item_data[segment]), axis=1 if n_channels == 2 else 0))
                     item_data[segment] /= norm_val
 
+            if self.enable_augmentation:
+                item_data = self.do_augmentation(item_data)
+
             fc = FeatureContainer()
             fc.feat = [item_data]
         else:
@@ -215,5 +232,111 @@ class RawAudioBatcher():
     def get_num_batches(self):
         if self.n_batches is None:
             self.n_batches = int(numpy.ceil(len(self.files)/self.batch_size))
+
         return self.n_batches
+
+    def create_augmentation_chain(self, augmentation_params):
+
+        deformers_list = []
+        chain_type = augmentation_params['chain_type']
+        chain_length = len(augmentation_params['config'])
+
+        if chain_type == 'Pipeline':
+            print('DATA AUGMENTATION WARNING: chain type muda.Pipeline not supported yet. Changing it to Union')
+            chain_type = 'Union'
+
+        self.augmentation_samples = 0
+        augmentation_config = augmentation_params['config']
+        for deformer_id, deformer_setup in enumerate(augmentation_config):
+            # deformer_setup = DottedDict(deformer_setup)
+
+            try:
+                DeformerClass = getattr(importlib.import_module("muda.deformers"), deformer_setup['class_name'])
+            except AttributeError:
+                message = '{name}: Invalid muda deformer class [{type}].'.format(
+                    name=self.__class__.__name__,
+                    type=deformer_setup['class_name']
+                )
+                self.logger.exception(message)
+                raise AttributeError(message)
+
+            if deformer_setup.get('config'):
+                deformer = DeformerClass(**dict(deformer_setup.get('config')))
+            else:
+                deformer = DeformerClass()
+
+            # create deformer tuple (name, deformer)
+            deformer_name = 'deformer_{}'.format(deformer_id)
+
+            # add a bypass in the first deformer, so we get the original input as first augmented output
+            if deformer_id == 0 or chain_type == 'Pipeline':
+                deformer = muda.deformers.Bypass(deformer)
+
+            deformers_list.append((deformer_name, deformer))
+
+            # comput number of augmentation samples
+            if 'n_samples' in deformer_setup['config']:
+                self.augmentation_samples += deformer_setup['config']['n_samples']
+            elif deformer_setup['class_name'] == 'DynamicRangeCompression':
+                self.augmentation_samples += len(deformer_setup['config']['preset'])
+            else:
+                self.augmentation_samples += 1
+
+
+
+
+
+        # compute number of created samples in augmentation
+        # self.augmentation_samples = \
+        #     numpy.sum([deformer_setup['config']['n_samples'] for deformer_setup in augmentation_params['config']])
+
+        # if chain_type == 'Pipeline':
+        #     self.augmentation_samples = numpy.sum([self.nCr(chain_length, group_size) for group_size in range(chain_length)])
+        # elif chain_type == 'Union':
+        #     pass
+
+        return getattr(importlib.import_module("muda"), augmentation_params['chain_type'])(steps=deformers_list)
+
+    def do_augmentation(self, item_data):
+
+        n_augmentations = self.augmentation_samples
+        n_segments = item_data.shape[0]
+        sgmt_duration = item_data.shape[1]
+        n_channels = item_data.shape[2]
+        augmented_data = numpy.zeros((n_segments + n_augmentations, sgmt_duration, n_channels))
+
+        # create a jam for each row and channel in item_data
+        for s_idx, segment in enumerate(item_data):
+            # jam = []
+            # for channel in range(numpy.shape(segment)[-1]):
+            for channel, sgmt_ch in enumerate(segment.T):
+
+                duration_smp = len(sgmt_ch)
+                duration_sec = duration_smp / self.desired_fs
+
+                jam = jams.JAMS()
+                jam.file_metadata.duration = duration_sec
+
+                j_orig = muda.jam_pack(jam, _audio=dict(y=sgmt_ch, sr=self.desired_fs))
+
+                for aug_idx, j_new in enumerate(self.augmentation_chain.transform(j_orig)):
+                    # for k in j_new['sandbox']['muda']['history']: print(k['transformer']['__class__'])
+
+                    # make sure output duration is the same as input
+                    augmented_audio = j_new['sandbox']['muda']['_audio']['y']
+
+                    len_diff = duration_smp - len(augmented_audio)
+                    if len_diff > 0:
+                        augmented_audio = numpy.concatenate((augmented_audio, numpy.zeros(len_diff)))
+                    if len_diff < 0:
+                        augmented_audio = augmented_audio[:len_diff]
+
+                    augmented_data[s_idx+aug_idx, :, channel] = augmented_audio
+
+        return augmented_data
+
+    def nCr(self, n, r):
+        import math
+        f = math.factorial
+        return f(n) // f(r) // f(n - r)
 
