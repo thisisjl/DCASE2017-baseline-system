967848489cea9cfdfdb9bb456b9b20ee5079646f
Jose Luis Diez Antich <jl.diez.antich@gmail.com>
2017-04-18 10:43:02 +0200

    learners and parameters for kernel param
    1. add kernel initialization parameters
    2. soundnet learner class has its own create model method which reads for kernel initialization

--- dcase_framework/learners.py
+++ dcase_framework/learners.py
@@ -1244,6 +1244,76 @@ class SceneClassifierSoundnet(SceneClassifier, KerasMixin):
         super(SceneClassifierSoundnet, self).__init__(*args, **kwargs)
         self.method = 'soundnet'
 
+    def create_model(self, input_shape):
+        from keras.models import Sequential
+        self.model = Sequential()
+        model_params = copy.deepcopy(self.learner_params.get_path('model.config'))
+
+        if 'conv_kernel' in self.learner_params['model']:
+            kernel_params = self.learner_params['model']['conv_kernel']
+            kernel = getattr(importlib.import_module("keras.initializers"), kernel_params['class'])
+            kernel = kernel(**kernel_params['config'])
+            set_kernel = True
+
+        for layer_id, layer_setup in enumerate(model_params):
+            layer_setup = DottedDict(layer_setup)
+            try:
+                LayerClass = getattr(importlib.import_module("keras.layers"), layer_setup['class_name'])
+            except AttributeError:
+                message = '{name}: Invalid Keras layer type [{type}].'.format(
+                    name=self.__class__.__name__,
+                    type=layer_setup['class_name']
+                )
+                self.logger.exception(message)
+                raise AttributeError(message)
+
+            if 'config' not in layer_setup:
+                layer_setup['config'] = {}
+
+            # Set layer input
+            if layer_id == 0 and layer_setup.get_path('config.input_shape') is None:
+                # Set input layer dimension for the first layer if not set
+                if layer_setup.get('class_name') == 'Dropout':
+                    layer_setup['config']['input_shape'] = (input_shape,)
+                else:
+                    layer_setup['config']['input_shape'] = input_shape
+
+            elif layer_setup.get_path('config.input_dim') == 'FEATURE_VECTOR_LENGTH':
+                layer_setup['config']['input_dim'] = input_shape
+
+            # Set layer output
+            if layer_setup.get_path('config.units') == 'CLASS_COUNT':
+                layer_setup['config']['units'] = len(self.class_labels)
+
+            # Set kernel initializer
+            if 'kernel_initializer' in layer_setup.get('config') and set_kernel:
+                layer_setup.get('config')['kernel_initializer'] = kernel
+
+
+            if layer_setup.get('config'):
+                self.model.add(LayerClass(**dict(layer_setup.get('config'))))
+            else:
+                self.model.add(LayerClass())
+
+        try:
+            OptimizerClass = getattr(importlib.import_module("keras.optimizers"),
+                                     self.learner_params.get_path('model.optimizer.type')
+                                     )
+
+        except AttributeError:
+            message = '{name}: Invalid Keras optimizer type [{type}].'.format(
+                name=self.__class__.__name__,
+                type=self.learner_params.get_path('model.optimizer.type')
+            )
+            self.logger.exception(message)
+            raise AttributeError(message)
+
+        self.model.compile(
+            loss=self.learner_params.get_path('model.loss'),
+            optimizer=OptimizerClass(**dict(self.learner_params.get_path('model.optimizer.parameters', {}))),
+            metrics=self.learner_params.get_path('model.metrics')
+        )
+
     def learn(self, data, annotations):
         """Learn based on data ana annotations
 
